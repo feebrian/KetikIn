8c658c1005e6d33ce403a4563c70d3cd98c899b1
feebrian <adamaditama13@gmail.com>
2023-07-11 00:25:58 +0700

    working on home view

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -6,9 +6,40 @@
             <br>
         @endauth
         <div class="max-w-3xl mx-auto">
-            <section id="article-container">
+            <section id="article-container w-[42.5rem]">
 
-                <article>
+                <article class="mb-8">
+                    <div class="flex place-items-center gap-x-2.5">
+                        <a href="#">
+                            <img src="static/images/gacor.png" alt="gacor" class="w-8 h-8 rounded-full">
+                        </a>
+
+                        <p class="text-sm">
+                            <a href="#">Adam Febrian Aditama</a><span class="text-slate-500"> - Feb 21, 2023</span>
+                        </p>
+                    </div>
+                    <div class="flex">
+                        <div class="block w-9/12">
+                            <a href="#">
+                                <h2 class="text-2xl font-bold my-1.5">Learn Tailwind is very easy</h2>
+                            </a>
+                            <a href="#">
+                                <div class="font-serif">
+                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil quibusdam soluta sint
+                                        facere nam aut labore, culpa eaque commodi vel impedit, blanditiis totam ducimus
+                                        modi dolores quidem nobis non fugiat!</p>
+                                </div>
+                            </a>
+                            <div class="flex gap-1 mt-1">
+                                <a href="#" class="badge badge-ghost">Programming</a>
+                            </div>
+                        </div>
+                        <div class="w-3/12 flex justify-end">
+                            <a href="#">
+                                <img src="static/images/gacor.png" alt="gacor kang" class="w-28 h-28">
+                            </a>
+                        </div>
+                    </div>
 
                 </article>
 
--- resources/views/template/partials/navbar.blade.php
+++ resources/views/template/partials/navbar.blade.php
@@ -13,6 +13,7 @@
                     <li><a href="#">Settings</a></li>
                     <li><a href="#">My Article</a></li>
                     <li><a href="#">Stats</a></li>
+                    <hr class="my-2">
                     <li>
                         <form action="/logout" method="POST">
                             @csrf
@@ -23,7 +24,7 @@
                 @guest
                     <li class="gap-y-1">
                     <li><a href="/signup">Register</a></li>
-                    <li><a href="/signin">Login</a></li>
+                    <li><a href="/login">Login</a></li>
                     </li>
                 @endguest
             </ul>
@@ -53,7 +54,7 @@
                 </div>
             </button>
             <button class="btn btn-ghost btn-circle">
-                <svg aria-hidden="true" fill="none" stroke="currentColor" stroke-width="2" class="h-6 w-6   "
+                <svg aria-hidden="true" fill="none" stroke="currentColor" stroke-width="2" class="h-5 w-5"
                     viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
